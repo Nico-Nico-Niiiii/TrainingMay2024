9f418162c145140476a34212cce5a28aa1ce1817
Nico-Nico-Niiiii <work.adityajha@gmail.com>
2024-05-23 05:24:33 +0000

    x X

---
+++ area_of_circle.c
@@ -0,0 +1,24 @@
+/*
+ * Area of circle program
+ * Author : Aditya 
+ * DOC ; 23rd may 2024
+ * */
+
+#include<stdio.h>
+
+int main(){
+
+	float pi=3.14;
+	float r;
+	float area;
+	printf("Enter the radius");
+	scanf("%f",&r);
+	area=pi*r*r;
+	printf("The area is %f ",area);
+	return 0;
+
+
+
+}
+
+
