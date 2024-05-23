7409a2ae4d80f121e4b80e782c2c82d571ea3d20
Nico-Nico-Niiiii <work.adityajha@gmail.com>
2024-05-23 05:14:09 +0000

    First version of hello world

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+/*
+ * Author: Aditya Narayan Jha
+ * Hello World Program
+ * DOC: 23rd May 2024
+ * */
+
+#include<stdio.h>
+int main(){
+	printf("This is my first program");
+	return 0;
+	
+
+
+}
+
